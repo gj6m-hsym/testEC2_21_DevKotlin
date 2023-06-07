64188428e09a057c0ea23359750ce8e7e16cc42d
gj6m-hsym <misaki.hoshiyama@asahinet.com>
2023-06-07 11:45:30 +0900

    push all File&Folder

--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # front end
-if [ -a /home/appdata/nuxtTest/server/index.mjs ]; then
-    node /home/appdata/nuxtTest/server/index.mjs &
-    sleep 5
-    curl http://localhost:3000 > /dev/null 2>&1
-    if [ "$?" != "0" ]; then
-        echo not running: 3000
-        exit 1
-    fi
-fi
+# if [ -a /home/appdata/nuxtTest/server/index.mjs ]; then
+#     node /home/appdata/nuxtTest/server/index.mjs &
+#     sleep 5
+#     curl http://localhost:3000 > /dev/null 2>&1
+#     if [ "$?" != "0" ]; then
+#         echo not running: 3000
+#         exit 1
+#     fi
+# fi
 
 # back end
 if [ -a /home/appdata/demo-0.0.1-SNAPSHOT.jar ]; then
